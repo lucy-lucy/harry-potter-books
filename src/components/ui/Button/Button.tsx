de6226892f46d4b660c4b00ff338735ee47c81f9
Liudmila Kotlyar <kotlyar.lyudmila.a@gmail.com>
2023-05-01 14:56:27 +0200

    Added Button component

---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from 'react';
+import styles from './Button.module.css';
+
+interface ButtonProps {
+    children: ReactNode;
+    onClick: () => void;
+}
+
+const Button = ({ children, onClick }: ButtonProps) => {
+    return (
+        <button className={styles.button} onClick={onClick}>
+            {children}
+        </button>
+    );
+};
+
+export default Button;
