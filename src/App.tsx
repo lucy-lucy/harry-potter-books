3a3c97e4a5a094001f8152c748c66936978c7d9f
Liudmila Kotlyar <kotlyar.lyudmila.a@gmail.com>
2023-05-01 14:57:24 +0200

    Adjusted layout of the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,43 @@
 import { useState } from 'react';
 import books from './api/books.json';
+import styles from './App.module.css';
+import BooksList from './components/feature/BooksList/BooksList';
 import { calculateCheapestPrice } from './utils/calculate-cheapest-price';
 
 const App = () => {
     const [cart, setCart] = useState<number[]>([]);
 
-    const handleAddClick = (id: number): () => void => {
-        return () => {
-            setCart((prevState) => [...prevState, id]);
-        };
+    const handleAddClick = (id: number): void => {
+        setCart((prevState) => [...prevState, id]);
     };
 
-    const handleRemoveClick = (id: number): () => void => {
-        return () => {
-            const nextCart = [...cart];
-            const lastCartItemIndex = cart.lastIndexOf(id);
+    const handleRemoveClick = (id: number): void => {
+        const nextCart = [...cart];
+        const lastCartItemIndex = cart.lastIndexOf(id);
 
-            if (lastCartItemIndex !== -1) {
-                nextCart.splice(lastCartItemIndex, 1);
-            }
+        if (lastCartItemIndex !== -1) {
+            nextCart.splice(lastCartItemIndex, 1);
+        }
 
-            setCart(nextCart);
-        };
+        setCart(nextCart);
     };
 
     return (
-        <>
-            <div>Harry Potter Books</div>
-            <ul>
-                {books.map((book) => <li key={book.id}>
-                    {book.id} - {book.title}
-                    <button onClick={handleAddClick(book.id)}>Add to cart</button>
-                    <button onClick={handleRemoveClick(book.id)}>Remove</button>
-                </li>)}
-            </ul>
-            <ul>
-                {cart.map((cartItem, index) => <li key={index}>{cartItem}</li>)}
-                Price: {calculateCheapestPrice(cart)}
-            </ul>
-        </>
+        <div className={styles.wrapper}>
+            <header className={styles.header}>
+                <h1>Harry Potter Books</h1>
+            </header>
+            <main className={styles.content}>
+                {books.length > 0 &&
+                    <BooksList books={books} onAddBook={handleAddClick} onRemoveBook={handleRemoveClick}/>}
+            </main>
+            <aside className={styles.aside}>
+                <ul>
+                    {cart.map((cartItem, index) => <li key={index}>{cartItem}</li>)}
+                    Price: {calculateCheapestPrice(cart)}
+                </ul>
+            </aside>
+        </div>
     );
 };
 
