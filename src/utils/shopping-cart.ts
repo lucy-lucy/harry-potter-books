3e8dacad18567ce5aad587b31d9dd40f1da81558
Liudmila Kotlyar <kotlyar.lyudmila.a@gmail.com>
2023-05-01 18:38:50 +0200

    Added a comment to the calculation logic

--- src/utils/shopping-cart.ts
+++ src/utils/shopping-cart.ts
@@ -27,12 +27,29 @@ const calculatePriceForCombinations = (combinations: number[][]): number => {
     }, 0);
 };
 
+/*
+To calculate the cheapest price we need to find the best combination of items (books). To do so, we need first:
+    1. Sort items by count, so the most occurring items go first, [1, 2, 2, 3, 4, 5, 5] -> [2, 2, 5, 5, 1, 3, 4]
+    2. Define the number of unique items, so we know the length of the largest combination -> 5
+    3. Build a matrix of combinations, starting from pairs and ending with combinations with length of the number of unique items
+    4. For every iteration calculate the current prices:
+        - book price * number of books * discount * number of combinations
+    5. If the current price is cheaper, set the current price as the cheapest price
+
+    Example:
+        i = 2: [ [2, 5], [2 ,5], [1, 3], [4] ] => 8 * 2 * 0.95 * 3 + 8  => price is 53.60
+        i = 3: [ [2, 5, 1], [2, 5, 4], [4] ] => 8 * 3 * 0.9 * 2 + 8 => price is 51.20
+        i = 4: [ [2, 5 ,1, 3], [2, 5, 4] ] => 8 * 4 * 0.8 + 8 * 3 * 0.9 => price is 47.20
+        i = 5: [ [2, 5, 1, 3, 4], [2, 5] ] => 8 * 5 * 0.75 + 8 * 2 * 0.95 => price is 45.20
+
+        The cheapest price is 45.20
+ */
 export const calculateCheapestPrice = (cartItems: CartItems): number => {
     const sortedCartItems = sortCartItemsByCount(cartItems);
-    const uniqueCartItemsCount = new Set(sortedCartItems).size;
+    const uniqueCartItemsNumber = new Set(sortedCartItems).size;
     let cheapestPrice = cartItems.length * BOOK_PRICE;
 
-    for (let i = 2; i <= uniqueCartItemsCount; i++) {
+    for (let i = 2; i <= uniqueCartItemsNumber; i++) {
         const tempSortedCartItems = [...sortedCartItems];
         let cartItemsCombinations: number[][] = [];
         let currentCombinationIndex = 0;
@@ -55,6 +72,8 @@ export const calculateCheapestPrice = (cartItems: CartItems): number => {
         }
 
         const currentPrice = calculatePriceForCombinations(cartItemsCombinations);
+
+        console.log(i, cartItemsCombinations, currentPrice.toFixed(2));
 
         if (currentPrice < cheapestPrice) {
             cheapestPrice = currentPrice;
